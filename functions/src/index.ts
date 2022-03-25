015e05f76c5cce12d55411b11885ae99d8b1cc74
Simranjeet Singh <smrnjeet222@gmail.com>
2022-03-25 12:50:06 +0530

    fix: add stats caching - remove serum price indexer

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -40,64 +40,6 @@ const connection = new web3.Connection('https://dawn-red-log.solana-mainnet.quik
 const provider = new anchor.Provider(connection, wallet, {})
 const cyclosCore = new anchor.Program(idl as anchor.Idl, PROGRAM_ID, provider)
 
-exports.addSerumPrices1 = functions
-  .region('asia-south1')
-  .pubsub
-  .schedule("*/15 * * * *").onRun(async () => {
-
-    async function getMarkets() {
-      try {
-        let mrktResp: any = await axios.get("https://cdn.jsdelivr.net/gh/solana-labs/token-list/src/tokens/solana.tokenlist.json")
-        let mrktData: any = await mrktResp.data
-        return mrktData.tokens.filter((f: any) => f.extensions?.serumV3Usdc || f.extensions?.serumV3Usdt)
-      } catch (err) {
-        console.log(err)
-        return []
-      }
-    }
-
-
-    async function getLatestPrice(
-      marketAddress: PublicKey
-    ): Promise<number> {
-      const market: Market = await Market.load(
-        CONNECTION,
-        marketAddress, {},
-        PROGRAMADDRESS
-      )
-
-      // Fetching orderbooks
-      const bids: Orderbook = await market.loadBids(CONNECTION)
-      const asks: Orderbook = await market.loadAsks(CONNECTION)
-      // L2 orderbook data
-      const high_bid: number = bids.getL2(1)?.[0]?.[0]
-      const low_ask: number = asks.getL2(1)?.[0]?.[0]
-
-      if (high_bid && low_ask) {
-        return (high_bid + low_ask) / 2
-      } else {
-        throw ("Could not fetch price")
-      }
-    }
-
-    const time = admin.firestore.Timestamp.now()
-
-    const filteredMarkets = await getMarkets()
-
-    await Promise.all(filteredMarkets.map(async (m: any) => {
-      try {
-        const price = await getLatestPrice(new PublicKey(m.extensions?.serumV3Usdc ?? m.extensions?.serumV3Usdt))
-        db.collection("serum-prices-1").doc(m.symbol).set({
-          address: m.extensions?.serumV3Usdc ?? m.extensions?.serumV3Usdt,
-          prices: admin.firestore.FieldValue.arrayUnion({ price, time }),
-        }, { merge: true })
-        console.log(`ADDED: ${m.symbol} @ ${price},  ${time.toDate()}`)
-      } catch (error) {
-        console.error(`\nFAILED ${m.symbol} => ${error}\n`)
-      }
-    }))
-    return
-  })
 
 exports.nft = functions
   .region('asia-south1')
@@ -264,7 +206,7 @@ exports.logTxs = functions
   })
   .region('asia-south1')
   .pubsub
-  .schedule("every 2 hours synchronized")
+  .schedule("*/30 * * * *")
   .onRun(async () => {
     try {
       // const tokenPrices = await (await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=solana%2Ccyclos%2Ctether%2Cusd-coin&vs_currencies=usd`)).data
@@ -276,11 +218,11 @@ exports.logTxs = functions
           const detailedInfo = await (await axios.get(`https://public-api.solscan.io/transaction/${txObj.txHash}`)).data
           for (const log of detailedInfo?.logMessage) {
             if (log.slice(0, 12) !== "Program log:") {
-              continue;
+              continue
             }
             decodedEvent = cyclosCore.coder.events.decode(log.slice(13))
             if (decodedEvent !== null) {
-              break;
+              break
             }
           }
           if (decodedEvent?.name === "SwapEvent") {
@@ -315,6 +257,164 @@ exports.logTxs = functions
     return
   })
 
+exports.statsCache = functions
+  .runWith({
+    // Ensure the function has enough memory and time
+    // to process large files
+    timeoutSeconds: 500,
+    // memory: "1GB",
+  })
+  .region('asia-south1')
+  .pubsub
+  .schedule("*/30 * * * *")
+  .onRun(async () => {
+    let last24hrVolume = 0
+    let TVL = 0
+    const volumePerPool = {}
+    const volumePerToken = {}
+    const analyticsRef = db.collection("swap-logs")
+    try {
+      let lastNsec = 86400_000
+      let user = null
+      // if (req.query?.lastNsec) {
+      //   lastNsec = parseInt(req.query.lastNsec as string) * 1000
+      // }
+      // if (req.query?.user) {
+      //   user = req.query.user as string
+      // }
+      const docs = user ?
+        lastNsec ?
+          await analyticsRef
+            .where("blockTime", ">", (new Date().getTime() - lastNsec) / 1000)
+            .where('signer', 'array-contains-any', [user])
+            .get()
+          :
+          await analyticsRef
+            .where('signer', 'array-contains-any', [user])
+            .get()
+        : lastNsec ?
+          await analyticsRef
+            .where("blockTime", ">", (new Date().getTime() - lastNsec) / 1000)
+            .get()
+          :
+          await analyticsRef.get()
+
+      const poolStates: any = await cyclosCore.account.poolState.all()
+      const tokenTVL: any = {}
+      const poolDetails: any = {}
+      const tokenDetails: any = {}
+
+      for (const pool of poolStates) {
+        const { token0, token1 } = pool.account
+
+        const ata0 = await SPLToken.Token.getAssociatedTokenAddress(
+          SPLToken.ASSOCIATED_TOKEN_PROGRAM_ID,
+          SPLToken.TOKEN_PROGRAM_ID,
+          token0,
+          pool.publicKey,
+          true
+        )
+        const ata1 = await SPLToken.Token.getAssociatedTokenAddress(
+          SPLToken.ASSOCIATED_TOKEN_PROGRAM_ID,
+          SPLToken.TOKEN_PROGRAM_ID,
+          token1,
+          pool.publicKey,
+          true
+        )
+
+        const ata0Info = await connection.getTokenAccountBalance(ata0)
+        const ata1Info = await connection.getTokenAccountBalance(ata1)
+
+        const poolAddress = pool.publicKey.toString()
+
+        console.log(pool)
+
+
+        poolDetails[poolAddress] = {
+          token0: pool.account.token0.toString(),
+          token1: pool.account.token1.toString(),
+          liqudity: pool.account.liquidity.toString(),
+          sqrtPriceX32: pool.account.sqrtPriceX32.toString(),
+          feeGrowthGlobal0X32: pool.account.feeGrowthGlobal0X32.toString(),
+          feeGrowthGlobal1X32: pool.account.feeGrowthGlobal1X32.toString(),
+          protocolFeesToken0: pool.account.protocolFeesToken0.toString(),
+          protocolFeesToken1: pool.account.protocolFeesToken1.toString(),
+          token0amount: ata0Info.value.uiAmount,
+          token1amount: ata1Info.value.uiAmount,
+        }
+
+        tokenTVL[token0] = tokenTVL[token0] ? tokenTVL[token0] + ata0Info.value.uiAmount : ata0Info.value.uiAmount
+        tokenTVL[token1] = tokenTVL[token1] ? tokenTVL[token1] + ata1Info.value.uiAmount : ata1Info.value.uiAmount
+      }
+
+      const coingekoEndpointArray = []
+
+      for (const token of Object.keys(tokenTVL)) {
+        try {
+          const tokenMeta = await (await axios.get(`https://public-api.solscan.io/token/meta?tokenAddress=${token}`))?.data
+          tokenDetails[token] = {
+            symbol: tokenMeta.symbol,
+            name: tokenMeta.name,
+            icon: tokenMeta.icon,
+            decimals: tokenMeta.decimals,
+            coingeckoId: tokenMeta.coingeckoId ? tokenMeta.coingeckoId : null,
+            tvl: tokenTVL[token],
+          }
+          coingekoEndpointArray.push(tokenMeta.coingeckoId)
+        } catch (err) {
+          console.log(err)
+        }
+      }
+
+      const coingeckoData = await (await axios.get(`https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=${coingekoEndpointArray.join("%2C")}`)).data
+
+      for (const token of Object.keys(tokenDetails)) {
+        const priceinUSD = coingeckoData[tokenDetails[token].coingeckoId]?.usd ?? 0
+        const tvlinUSD = tokenDetails[token].tvl * priceinUSD
+        tokenDetails[token] = {
+          ...tokenDetails[token],
+          priceinUSD,
+          tvlinUSD,
+        }
+        TVL += tvlinUSD
+      }
+
+      for (const pool of Object.keys(poolDetails)) {
+        const data = poolDetails[pool]
+        poolDetails[pool] = {
+          ...data,
+          tvlinUSD: data.token0amount * (tokenDetails?.[data.token0.toString()]?.priceinUSD ?? 0)
+            + data.token1amount * (tokenDetails?.[data.token1.toString()]?.priceinUSD ?? 0)
+        }
+      }
+
+      for (const doc of docs.docs) {
+        const txData = doc.data()
+        const poolAddress = txData.poolState
+        const poolInfo: any = poolDetails[poolAddress]
+        if (poolInfo) {
+          const token0 = poolInfo.token0.toString()
+          const token1 = poolInfo.token1.toString()
+          volumePerToken[token0] = volumePerToken[token0] ?
+            volumePerToken[token0] + txData.tradeValue : txData.tradeValue
+
+          volumePerToken[token1] = volumePerToken[token1] ?
+            volumePerToken[token1] + txData.tradeValue : txData.tradeValue
+        }
+        volumePerPool[poolAddress] = volumePerPool[poolAddress] ? volumePerPool[poolAddress] + txData.tradeValue : txData.tradeValue
+        last24hrVolume += txData.tradeValue
+      }
+      db.collection("stats-cache").doc("latest").set({
+        last24hrVolume, volumePerPool, volumePerToken, poolDetails, tokenDetails, TVL
+      }, { merge: true })
+      // res.status(200).send({ last24hrVolume, volumePerPool, volumePerToken, poolDetails, tokenDetails, TVL })
+    } catch (err) {
+      console.log(err)
+    }
+
+  })
+
+
 exports.stats = functions
   .region('asia-south1')
   .https.onRequest((req, res) => {
@@ -323,141 +423,11 @@ exports.stats = functions
       return
     }
     cors(req, res, async () => {
-      let last24hrVolume = 0
-      let TVL = 0
-      const volumePerPool = {}
-      const volumePerToken = {}
-      const analyticsRef = db.collection("swap-logs")
       try {
-        let lastNsec = 86400_000
-        let user = null
-        if (req.query?.lastNsec) {
-          lastNsec = parseInt(req.query.lastNsec as string) * 1000
-        }
-        if (req.query?.user) {
-          user = req.query.user as string
-        }
-        const docs = user ?
-          lastNsec ?
-            await analyticsRef
-              .where("blockTime", ">", (new Date().getTime() - lastNsec) / 1000)
-              .where('signer', 'array-contains-any', [user])
-              .get()
-            :
-            await analyticsRef
-              .where('signer', 'array-contains-any', [user])
-              .get()
-          : lastNsec ?
-            await analyticsRef
-              .where("blockTime", ">", (new Date().getTime() - lastNsec) / 1000)
-              .get()
-            :
-            await analyticsRef.get()
-
-        const poolStates: any = await cyclosCore.account.poolState.all()
-        const tokenTVL: any = {}
-        const poolDetails: any = {}
-        const tokenDetails: any = {}
-
-        for (const pool of poolStates) {
-          const { token0, token1 } = pool.account
-
-          const ata0 = await SPLToken.Token.getAssociatedTokenAddress(
-            SPLToken.ASSOCIATED_TOKEN_PROGRAM_ID,
-            SPLToken.TOKEN_PROGRAM_ID,
-            token0,
-            pool.publicKey,
-            true
-          )
-          const ata1 = await SPLToken.Token.getAssociatedTokenAddress(
-            SPLToken.ASSOCIATED_TOKEN_PROGRAM_ID,
-            SPLToken.TOKEN_PROGRAM_ID,
-            token1,
-            pool.publicKey,
-            true
-          )
-
-          const ata0Info = await connection.getTokenAccountBalance(ata0)
-          const ata1Info = await connection.getTokenAccountBalance(ata1)
-
-          const poolAddress = pool.publicKey.toString()
-
-          poolDetails[poolAddress] = {
-            ...pool.account,
-            token0: pool.account.token0.toString(),
-            token1: pool.account.token1.toString(),
-            liqudity: pool.account.liquidity.toString(),
-            sqrtPriceX32: pool.account.sqrtPriceX32.toString(),
-            feeGrowthGlobal0X32: pool.account.feeGrowthGlobal0X32.toString(),
-            feeGrowthGlobal1X32: pool.account.feeGrowthGlobal1X32.toString(),
-            protocolFeesToken0: pool.account.protocolFeesToken0.toString(),
-            protocolFeesToken1: pool.account.protocolFeesToken1.toString(),
-            token0amount: ata0Info.value.uiAmount,
-            token1amount: ata1Info.value.uiAmount,
-          }
-
-          tokenTVL[token0] = tokenTVL[token0] ? tokenTVL[token0] + ata0Info.value.uiAmount : ata0Info.value.uiAmount
-          tokenTVL[token1] = tokenTVL[token1] ? tokenTVL[token1] + ata1Info.value.uiAmount : ata1Info.value.uiAmount
-        }
-
-        const coingekoEndpointArray = []
-
-        for (const token of Object.keys(tokenTVL)) {
-          try {
-            const tokenMeta = await (await axios.get(`https://public-api.solscan.io/token/meta?tokenAddress=${token}`))?.data
-            tokenDetails[token] = {
-              symbol: tokenMeta.symbol,
-              name: tokenMeta.name,
-              icon: tokenMeta.icon,
-              decimals: tokenMeta.decimals,
-              coingeckoId: tokenMeta.coingeckoId ? tokenMeta.coingeckoId : null,
-              tvl: tokenTVL[token],
-            }
-            coingekoEndpointArray.push(tokenMeta.coingeckoId)
-          } catch (err) {
-            console.log(err)
-          }
-        }
-
-        const coingeckoData = await (await axios.get(`https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=${coingekoEndpointArray.join("%2C")}`)).data
-
-        for (const token of Object.keys(tokenDetails)) {
-          const priceinUSD = coingeckoData[tokenDetails[token].coingeckoId]?.usd ?? 0
-          const tvlinUSD = tokenDetails[token].tvl * priceinUSD
-          tokenDetails[token] = {
-            ...tokenDetails[token],
-            priceinUSD,
-            tvlinUSD,
-          }
-          TVL += tvlinUSD
-        }
-
-        for (const pool of Object.keys(poolDetails)) {
-          const data = poolDetails[pool]
-          poolDetails[pool] = {
-            ...data,
-            tvlinUSD: data.token0amount * (tokenDetails?.[data.token0.toString()]?.priceinUSD ?? 0)
-              + data.token1amount * (tokenDetails?.[data.token1.toString()]?.priceinUSD ?? 0)
-          }
-        }
-
-        for (const doc of docs.docs) {
-          const txData = doc.data()
-          const poolAddress = txData.poolState
-          const poolInfo: any = poolDetails[poolAddress]
-          if (poolInfo) {
-            const token0 = poolInfo.token0.toString()
-            const token1 = poolInfo.token1.toString()
-            volumePerToken[token0] = volumePerToken[token0] ?
-              volumePerToken[token0] + txData.tradeValue : txData.tradeValue
-
-            volumePerToken[token1] = volumePerToken[token1] ?
-              volumePerToken[token1] + txData.tradeValue : txData.tradeValue
-          }
-          volumePerPool[poolAddress] = volumePerPool[poolAddress] ? volumePerPool[poolAddress] + txData.tradeValue : txData.tradeValue
-          last24hrVolume += txData.tradeValue
-        }
-        res.status(200).send({ last24hrVolume, volumePerPool, volumePerToken, poolDetails, tokenDetails, TVL })
+        const analyticsRef = db.collection("stats-cache")
+        const docs = await analyticsRef.doc("latest").get()
+        const data = docs.data()
+        res.status(200).send(data)
       } catch (err) {
         console.log(err)
         res.status(200).send("Something went wrong")
